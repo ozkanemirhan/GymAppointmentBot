db83defa71e384285b325ef929dcc613bbeb1d9c
emirhan.ozkan <emirhan.ozkan@accenture.com>
2023-08-05 15:31:54 +0300

    Reached up to UyeseansSecim page

--- GymAppointmentBot.py
+++ GymAppointmentBot.py
@@ -1,5 +1,6 @@
 # Import necessary libraries
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,34 +11,90 @@ from selenium.webdriver.support import expected_conditions as EC
 
 # Function to log in to the website
 def login(username, password):
-    # Initialize the web driver (Make sure you have the appropriate web driver executable for your browser)
-    chrome_driver_path = 'C:\Users\emirhan.ozkan\Documents\personal\GymAppointmentBot\chromedriver.exe'
-    driver = webdriver.Chrome(executable_path=chrome_driver_path)
+    # Initialize the web driver (Make sure you have the appropriate web driver
+    # executable for your browser)
+    chrome_driver_path = (
+        'C:\\Users\\emirhan.ozkan\\Documents\\personal\\' +
+        'GymAppointmentBot\\chromedriver.exe')
 
-    # Navigate to the login page
-    driver.get('https://online.spor.istanbul/uyegiris')
+    # Initialize the ChromeDriver service with the executable path
+    service = webdriver.chrome.service.Service(
+        executable_path=chrome_driver_path)
+    service.start()
 
-    # Find and fill in the login form
-    username_field = driver.find_element(By.ID, 'txtTCPasaport')
-    password_field = driver.find_element(By.ID, 'txtSifre')
-    username_field.send_keys(username)
-    password_field.send_keys(password)
-    password_field.send_keys(Keys.RETURN)
+    # Initialize ChromeOptions and set necessary preferences
+    chrome_options = webdriver.ChromeOptions()
+    # You can add more preferences if needed
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--disable-features=CookieSameSite")
+    chrome_options.add_experimental_option('prefs', {
+        'profile.default_content_setting_values.cookies': 1
+    })
 
-    # Wait for the login to be successful (you might need to add more waiting time or improve this logic)
-    WebDriverWait(driver, 10).until(EC.url_to_be(
-        'https://online.spor.istanbul/anasayfa'))
+    # Initialize the web driver
+    driver = webdriver.Remote(service.service_url, options=chrome_options)
+
+    try:
+        # Navigate to the login page
+        driver.get('https://online.spor.istanbul/uyegiris')
+
+        # Find and fill in the login form
+        username_field = driver.find_element(By.ID, 'txtTCPasaport')
+        password_field = driver.find_element(By.ID, 'txtSifre')
+        username_field.send_keys(username)
+        password_field.send_keys(password)
+        password_field.send_keys(Keys.RETURN)
+
+        # Wait for the login to be successful
+        print("Waiting for the login to be successful...")
+        WebDriverWait(driver, 10).until(EC.url_to_be(
+            'https://online.spor.istanbul/anasayfa'))
+        print("Login successful. Current URL: ", driver.current_url)
+
+        # Clicking uyespor page
+        print("Waiting for the uyespor page to load...")
+        driver.get('https://online.spor.istanbul/uyespor')
+        print(
+            "Successfully clicked uyespor. Current URL: ", driver.current_url)
+
+        # Wait for the element to be present
+        element_id = 'pageContent_rptListe_lbtnSeansSecim_0'
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            (By.ID, element_id)))
+
+        # Execute the JavaScript function to trigger the click
+        script = f"document.getElementById('{element_id}').click();"
+        driver.execute_script(script)
+
+        print("Element successfully clicked")
+
+        # Wait for the uyeseanssecim to be successful
+        print("Waiting for the uyeseanssecim to be successful...")
+        WebDriverWait(driver, 10).until(EC.url_to_be(
+            'https://online.spor.istanbul/uyeseanssecim'))
+        print("Uyeseanssecim successful. Current URL: ", driver.current_url)
+
+    except (TimeoutException, NoSuchElementException) as e:
+        print("Error occurred during login:", str(e))
+        # Handle the exception here if needed
 
     return driver
 
-
+"""
 def get_appointments(driver):
     # Navigate to the appointments page
-    driver.get('https://online.spor.istanbul/appointments')
+    print("Waiting for the uyeseanssecim.aspx page to load...")
+    driver.get('https://online.spor.istanbul/uyeseanssecim.aspx')
+    print("Successful. Current URL: ", driver.current_url)
+
+    print("Waiting for the uyeseanssecim to be successful...")
+    WebDriverWait(driver, 10).until(EC.url_to_be(
+        'https://online.spor.istanbul/uyeseanssecim'))
+    print("Successful. Current URL: ", driver.current_url)
 
     # Wait for the page to load (you might need to adjust the waiting time)
     WebDriverWait(driver, 10).until(EC.url_to_be(
-        'https://online.spor.istanbul/appointments'))
+        'https://online.spor.istanbul/uyeseanssecim'))
 
     # Assuming the appointments are listed in a table, find the table element
     appointments_table = driver.find_element(By.ID, 'appointments-table')
@@ -50,7 +107,8 @@ def get_appointments(driver):
         date_cell, time_cell = row.find_elements(By.TAG_NAME, 'td')[:2]
         date = date_cell.text.strip()
         time = time_cell.text.strip()
-        # Check if the appointment is available (you can add more conditions here based on the website's structure)
+        # Check if the appointment is available (you can add more conditions
+        # here based on the website's structure)
         if 'Available' in time:
             available_appointments.append(f'{date} - {time}')
 
@@ -92,47 +150,20 @@ def reserve_appointment(driver, appointment_time):
         print("Appointment reserved successfully!")
     else:
         print("Failed to reserve appointment.")
-
-
 """
-# Function to get available gym appointments
-def get_appointments(driver):
-    # Navigate to the gym schedule page
-    driver.get('https://online.spor.istanbul/uyeseanssecim')
-
-    # Wait for the page to load and display the schedule (you might need to adjust the waiting time)
-    time.sleep(5)
-
-    # Extract the page source and create a BeautifulSoup object for parsing
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-    # Use BeautifulSoup to find and extract available appointment times
-    # You'll need to inspect the page source and find the appropriate HTML elements for this.
-    # It might be a table, a list, or some other element containing the appointment details.
-
-    return available_appointments
-
-# Function to reserve an appointment
-def reserve_appointment(driver, appointment_time):
-    # Implement the logic to select and reserve the desired appointment time.
-    # This will depend on the website's specific interface for reserving appointments.
-    # You might need to interact with drop-down menus, buttons, or forms.
-
-    # After reserving the appointment, you might want to add some logic to confirm the reservation was successful.
-
-"""
-
 # Main function to run the bot
 
 
 def main():
-    username = '35476323148'
-    password = 'Emirhan244'
+    username = '*****'
+    password = '*****'
 
     try:
         # Log in to the website
+        driver = webdriver.Chrome()
         driver = login(username, password)
 
+        """
         # Get available appointments
         available_appointments = get_appointments(driver)
 
@@ -141,6 +172,7 @@ def main():
             # You can modify this logic to select the appointment you prefer
             appointment_time = available_appointments[0]
             reserve_appointment(driver, appointment_time)
+        """
 
     finally:
         # Close the web driver after the script finishes
